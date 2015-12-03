7e982a3244634b9ff25690f5316e2a354019be7e
sdce.gaoxy@gmail.com <sdce.gaoxy@gmail.com>
2015-12-02 18:44:46 -0800

    optimize for the loop code

--- vectacorp/js/part2.js
+++ vectacorp/js/part2.js
@@ -1,46 +1,51 @@
-$(function(){
-    var _Data="";
-    
+var ObjMap=new Map();
+var _Data="";
+
+$(document).bind('pageinit',function(){ 
    $.getJSON( "js/data.json", function( data ) {
     var html="";
     _Data=data;
      for(var i=0;i<data.length;i++){
        var obj=data[i];
-         
+       ObjMap.set(obj.id,obj); 
+		 
         html+="<li><a href=\"#details\" uid=\""+obj.id+"\">"
                 +"<img src=\""+obj.imagepath+"\">"
                 +"<h2>"+obj.name.first+"&nbsp;&nbsp;"+obj.name.last+"</h2>"
                 +"<p>"+obj.title+"</p></a></li>" ;
      }
      $("#homelist ul").html(html); 
-     $('#homelist ul').listview('refresh');
+     $('#homelist ul').listview().listview('refresh');
   });
         
 
     $("#homelist").on("click",'a',function(){
         var user_id=$(this).attr("uid");
-        var imgpath="";
-        var name="";
-        var title="";
-        
-        for(var i=0;i<_Data.length;i++){
-            var obj=_Data[i];
-            if(obj.id==user_id){
-               $("#details-info").html(
-               "<li><img src=\""+obj.imagepath+"\"></li>"
-                +"<li>"+obj.name.first+"&nbsp;&nbsp;"
-                +obj.name.last+"</li>"
-                +"<li>"+obj.title+"</li>"
-               );
-             $("#details-id-list").html(   
-             "<li><a href=\"#details\"><h2>Veiw Manager</h2><p>"+obj.reportsto+"</p></a></li>"
-            +"<li><a href=\"#reporters\"><h2>Veiw Direct Reports</h2><p>2</p></a></li>"
-            +"<li><a href=\"#\"><h2>Call Office</h2><p>"+obj.officenumber+"</p></a></li>"
-            +"<li><a href=\"#\"><h2>Call Cell</h2><p>"+obj.cellnumber+"</p></a></li>"
-              );
-            }
-        }
-         $('#details-id-list').listview('refresh');
+		var obj=ObjMap.get(user_id);
+		var reportname=getUserName(obj.reportsto);
+		$("#details-info img").attr("src",obj.imagepath);
+		
+        var html=$("#details-info").html();
+		html=html.replace("{{name}}",obj.name.first+"  "+obj.name.last);
+		html=html.replace("{{title}}",obj.title);
+ 		$("#details-info").html(html);
+		
+		var listhtml=$("#details-id-list").html();
+		listhtml=listhtml.replace("{{vm}}",reportname);
+		listhtml=listhtml.replace("{{officenumber}}",obj.officenumber);
+		listhtml=listhtml.replace("{{cellnumber}}",obj.cellnumber);
+		$("#details-id-list").html(listhtml);
+        $('#details-id-list').listview().listview('refresh');
    });
     
 });
+
+function getUserName(id){
+
+	var fullname="";
+	var obj=ObjMap.get(id);
+	if(obj==null)return "";
+	fullname=obj.name.first+"  "+obj.name.last;
+	
+	return fullname;
+}
